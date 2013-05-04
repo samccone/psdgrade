727ec0b1ba4492632470e1848e14fc2e80179bf0
Sam Saccone <samccone@gmail.com>
2013-05-04 16:23:45 -0400

    use image magic for layer name extraction

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
-var psd = require("psd").PSD;
+var util  = require('util'),
+    exec = require('child_process').exec;
 
 exports.set = function(app) {
   app.get('/', homePage);
@@ -14,12 +15,22 @@ function homePage(req, res) {
 }
 
 function uploadPSD(req, res) {
-  var toParse = psd.fromFile(req.files.psd.path);
-  try {
-    toParse.parse();
-    res.render("uploaded", {data: toParse.toJSON().layerMask.layers});
-  } catch(e) {
-    res.render("uploaded", {error: e});
-  }
+  var layers = [];
+  var fileName = "/Users/sam/Dropbox/Internal/MojoTech.com/4.0/PSD/Mojo-Contact.psd"
 
+  exec('convert '+fileName+' -format "%[scenes]" info: | head -n 1', function(e, stdout, stderr) {
+      getLayerInfo(fileName, parseInt(stdout));
+  });
+
+  function getLayerInfo(fileName, totalCount) {
+    exec('convert '+fileName+'['+layers.length+'] -verbose info: | grep "label:"', function(e, stdout, stderr) {
+      layers.push(stdout.split("label: ")[1]);
+      console.log("found "+ layers[layers.length -1]);
+      if (layers.length < totalCount) {
+        getLayerInfo(fileName, totalCount);
+      } else {
+        res.render("uploaded", {error: null, data: layers});
+      }
+    });
+  }
 }
